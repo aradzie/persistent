48c9308aecd3af2c19bc35f5981f491ef667a8e9
Alex Radzie <aradzivanovich@gmail.com>
2011-07-04 20:21:25 +0300

    Added few more tests

--- src/test/java/collection/FingerTreeSeqTest.java
+++ src/test/java/collection/FingerTreeSeqTest.java
@@ -1,27 +1,24 @@
 package collection;
 
-import debug.IndentingPrintWriter;
 import org.junit.Ignore;
 import org.junit.Test;
-
-import java.io.OutputStreamWriter;
 
 import static org.junit.Assert.*;
 
 public class FingerTreeSeqTest {
   @Test(expected = Seq.EmptyException.class)
-  public void head() throws Exception {
+  public void headOfEmptySeq() throws Exception {
     new FingerTreeSeq<String>().head();
   }
 
   @Ignore
   @Test(expected = Seq.EmptyException.class)
-  public void tail() throws Exception {
+  public void tailOfEmptySeq() throws Exception {
     new FingerTreeSeq<String>().tail();
   }
 
   @Test
-  public void test() {
+  public void cons() {
     FingerTreeSeq<Integer> t = new FingerTreeSeq<Integer>();
     for (int n = 0; n < 1000; n++) {
       t = t.cons(n);
@@ -31,6 +28,20 @@ public class FingerTreeSeqTest {
         assertEquals(m, (int) t.nth(m));
       }
     }
-    t.dump(new IndentingPrintWriter(new OutputStreamWriter(System.out)));
+    //t.dump(new IndentingPrintWriter(new OutputStreamWriter(System.out)));
+  }
+
+  @Test
+  public void snoc() {
+    FingerTreeSeq<Integer> t = new FingerTreeSeq<Integer>();
+    for (int n = 0; n < 1000; n++) {
+      t = t.snoc(n);
+      assertEquals(0, (int) t.head());
+      assertEquals(n + 1, t.size());
+      for (int m = 0; m < t.size(); m++) {
+        assertEquals(n - m, (int) t.nth(m));
+      }
+    }
+    //t.dump(new IndentingPrintWriter(new OutputStreamWriter(System.out)));
   }
 }
