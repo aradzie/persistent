1b9dfc45fa9af80db943d3f5bbf00bc6fd9df20d
Alex Radzie <aradzivanovich@gmail.com>
2011-07-07 13:25:00 +0300

    Added persistent stack

---
+++ src/main/java/collection/Stack.java
@@ -0,0 +1,40 @@
+package collection;
+
+import java.util.NoSuchElementException;
+
+public class Stack<T> {
+  private final T v;
+  private final Stack<T> next;
+
+  public Stack() {
+    v = null;
+    next = null;
+  }
+
+  private Stack(T v, Stack<T> next) {
+    this.v = v;
+    this.next = next;
+  }
+
+  public Stack<T> push(T v) {
+    return new Stack<T>(v, this);
+  }
+
+  public boolean isEmpty() {
+    return next == null;
+  }
+
+  public T peek() {
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    }
+    return v;
+  }
+
+  public Stack<T> pop() {
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    }
+    return next;
+  }
+}
