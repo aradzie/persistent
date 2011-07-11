8b7f825c771726ab17fc2bfab2e3bb38ff66dd76
Alex Radzy <aradzivanovich@gmail.com>
2011-07-11 15:45:44 +0300

    Added persistent queue

---
+++ src/main/java/collection/Queue.java
@@ -0,0 +1,69 @@
+package collection;
+
+import java.util.NoSuchElementException;
+
+/**
+ * Trivial persistent queue implementation
+ * in an imperative language with eager evaluation.
+ *
+ * @param <T> Element type.
+ */
+public final class Queue<T> {
+  private final Stack<T> b;
+  private final Stack<T> f;
+
+  public Queue() {
+    b = f = new Stack<T>();
+  }
+
+  private Queue(Stack<T> b, Stack<T> f) {
+    this.b = b;
+    this.f = f;
+  }
+
+  /** @return Number of elements in this queue instance. */
+  public int size() {
+    return b.size() + f.size();
+  }
+
+  /**
+   * @return <code>true</code> if this queue is empty,
+   *         <code>false</code> otherwise.
+   */
+  public boolean isEmpty() {
+    return b.isEmpty() && f.isEmpty();
+  }
+
+  public Queue<T> push(T v) {
+    if (isEmpty()) {
+      return new Queue<T>(b, f.push(v));
+    }
+    else {
+      return new Queue<T>(b.push(v), f);
+    }
+  }
+
+  public T peek() {
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    }
+    if (f.isEmpty()) {
+      return b.reverse().peek();
+    }
+    else {
+      return f.peek();
+    }
+  }
+
+  public Queue<T> pop() {
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    }
+    if (f.isEmpty()) {
+      return new Queue<T>(f, b.reverse().pop());
+    }
+    else {
+      return new Queue<T>(b, f.pop());
+    }
+  }
+}
--- src/main/java/collection/Stack.java
+++ src/main/java/collection/Stack.java
@@ -2,28 +2,54 @@ package collection;
 
 import java.util.NoSuchElementException;
 
+/**
+ * Trivial persistent stack implementation.
+ *
+ * @param <T> Element type.
+ */
 public final class Stack<T> {
   private final T v;
   private final Stack<T> next;
+  private final int size;
 
+  /** Create new empty stack instance. */
   public Stack() {
     v = null;
     next = null;
+    size = 0;
   }
 
   private Stack(T v, Stack<T> next) {
     this.v = v;
     this.next = next;
+    this.size = 1 + this.next.size();
   }
 
+  /** @return Number of elements in this stack instance. */
+  public int size() {
+    return size;
+  }
+
+  /**
+   * @return <code>true</code> if this stack is empty,
+   *         <code>false</code> otherwise.
+   */
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  /**
+   * @param v An element to push to the stack.
+   * @return New stack instance with the pushed element on top.
+   */
   public Stack<T> push(T v) {
     return new Stack<T>(v, this);
   }
 
-  public boolean isEmpty() {
-    return next == null;
-  }
-
+  /**
+   * @return The topmost element on this stack.
+   * @throws NoSuchElementException If stack is empty.
+   */
   public T peek() {
     if (isEmpty()) {
       throw new NoSuchElementException();
@@ -31,10 +57,31 @@ public final class Stack<T> {
     return v;
   }
 
+  /**
+   * @return New stack instance without the topmost element.
+   * @throws NoSuchElementException If stack is empty.
+   */
   public Stack<T> pop() {
     if (isEmpty()) {
       throw new NoSuchElementException();
     }
     return next;
+  }
+
+  /**
+   * @return New stack instance with the same elements
+   *         but with reversed order.
+   */
+  public Stack<T> reverse() {
+    if (isEmpty()) {
+      return this;
+    }
+    else {
+      Stack<T> r = new Stack<T>();
+      for (Stack<T> s = this; !s.isEmpty(); s = s.pop()) {
+        r = r.push(s.peek());
+      }
+      return r;
+    }
   }
 }
