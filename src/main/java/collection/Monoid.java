9c2cbf2396dc34967e6ee009e0d712c591b76ed3
Alex Radzy <aradzivanovich@gmail.com>
2011-07-12 11:44:13 +0300

    Added few javadocs to the Monoid class

--- src/main/java/collection/Monoid.java
+++ src/main/java/collection/Monoid.java
@@ -1,11 +1,31 @@
 package collection;
 
 public interface Monoid<T extends Monoid<T>> {
+  /**
+   * @return An identity value that is when combined with any
+   *         other value is just that other value.
+   */
   T unit();
 
+  /**
+   * An associative combining operation. The operation must obey
+   * the following rules in order for a class to be a proper monoid:
+   * <ul>
+   * <li>assert m.combine(m.unit()).equals(m);</li>
+   * <li>assert m.unit().combine(m).equals(m);</li>
+   * <li>assert a.combine(b).combine(c).equals(a.combine(b.combine(c)));</li>
+   * </ul>
+   *
+   * @param that Other monoid value.
+   * @return Combined monoid value.
+   */
   T combine(T that);
 
-  class StringConcat implements Monoid<StringConcat>, CharSequence {
+  /**
+   * A specific monoid instance over set of strings with empty string as the unit
+   * and concatenation as the combining operation.
+   */
+  final class StringConcat implements Monoid<StringConcat>, CharSequence {
     public static final StringConcat UNIT = new StringConcat();
     private final String v;
 
@@ -24,6 +44,12 @@ public interface Monoid<T extends Monoid<T>> {
 
     @Override
     public StringConcat combine(StringConcat that) {
+      if (this == UNIT) {
+        return that;
+      }
+      if (that == UNIT) {
+        return this;
+      }
       return new StringConcat(v + that.v);
     }
 
@@ -41,9 +67,26 @@ public interface Monoid<T extends Monoid<T>> {
     public CharSequence subSequence(int start, int end) {
       return v.subSequence(start, end);
     }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) { return true; }
+      if (!(o instanceof StringConcat)) { return false; }
+      StringConcat that = (StringConcat) o;
+      return v.equals(that.v);
+    }
+
+    @Override
+    public int hashCode() {
+      return v.hashCode();
+    }
   }
 
-  class IntegerSum extends Number implements Monoid<IntegerSum> {
+  /**
+   * A specific monoid instance over set of integers with 0 (zero) as the unit
+   * and summation as the combining operation.
+   */
+  final class IntegerSum extends Number implements Monoid<IntegerSum> {
     public static final IntegerSum UNIT = new IntegerSum();
     private final int v;
 
@@ -62,6 +105,12 @@ public interface Monoid<T extends Monoid<T>> {
 
     @Override
     public IntegerSum combine(IntegerSum that) {
+      if (this == UNIT) {
+        return that;
+      }
+      if (that == UNIT) {
+        return this;
+      }
       return new IntegerSum(v + that.v);
     }
 
@@ -84,9 +133,26 @@ public interface Monoid<T extends Monoid<T>> {
     public double doubleValue() {
       return v;
     }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) { return true; }
+      if (!(o instanceof IntegerSum)) { return false; }
+      IntegerSum that = (IntegerSum) o;
+      return v == that.v;
+    }
+
+    @Override
+    public int hashCode() {
+      return v;
+    }
   }
 
-  class IntegerMul extends Number implements Monoid<IntegerMul> {
+  /**
+   * A specific monoid instance over set of integers with 1 (one) as the unit
+   * and multiplication as the combining operation.
+   */
+  final class IntegerMul extends Number implements Monoid<IntegerMul> {
     public static final IntegerMul UNIT = new IntegerMul();
     private final int v;
 
@@ -105,6 +171,12 @@ public interface Monoid<T extends Monoid<T>> {
 
     @Override
     public IntegerMul combine(IntegerMul that) {
+      if (this == UNIT) {
+        return that;
+      }
+      if (that == UNIT) {
+        return this;
+      }
       return new IntegerMul(v * that.v);
     }
 
@@ -127,9 +199,26 @@ public interface Monoid<T extends Monoid<T>> {
     public double doubleValue() {
       return v;
     }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) { return true; }
+      if (!(o instanceof IntegerMul)) { return false; }
+      IntegerMul that = (IntegerMul) o;
+      return v == that.v;
+    }
+
+    @Override
+    public int hashCode() {
+      return v;
+    }
   }
 
-  class IntegerMin extends Number implements Monoid<IntegerMin> {
+  /**
+   * A specific monoid instance over set of integers with maximal value as the unit
+   * and minimal element selection as the combining operation.
+   */
+  final class IntegerMin extends Number implements Monoid<IntegerMin> {
     public static final IntegerMin UNIT = new IntegerMin();
     private final int v;
 
@@ -148,6 +237,12 @@ public interface Monoid<T extends Monoid<T>> {
 
     @Override
     public IntegerMin combine(IntegerMin that) {
+      if (this == UNIT) {
+        return that;
+      }
+      if (that == UNIT) {
+        return this;
+      }
       return new IntegerMin(Math.min(v, that.v));
     }
 
@@ -170,9 +265,26 @@ public interface Monoid<T extends Monoid<T>> {
     public double doubleValue() {
       return v;
     }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) { return true; }
+      if (!(o instanceof IntegerMin)) { return false; }
+      IntegerMin that = (IntegerMin) o;
+      return v == that.v;
+    }
+
+    @Override
+    public int hashCode() {
+      return v;
+    }
   }
 
-  class IntegerMax extends Number implements Monoid<IntegerMax> {
+  /**
+   * A specific monoid instance over set of integers with minimal value as the unit
+   * and maximal element selection as the combining operation.
+   */
+  final class IntegerMax extends Number implements Monoid<IntegerMax> {
     public static final IntegerMax UNIT = new IntegerMax();
     private final int v;
 
@@ -191,6 +303,12 @@ public interface Monoid<T extends Monoid<T>> {
 
     @Override
     public IntegerMax combine(IntegerMax that) {
+      if (this == UNIT) {
+        return that;
+      }
+      if (that == UNIT) {
+        return this;
+      }
       return new IntegerMax(Math.max(v, that.v));
     }
 
@@ -211,6 +329,19 @@ public interface Monoid<T extends Monoid<T>> {
 
     @Override
     public double doubleValue() {
+      return v;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) { return true; }
+      if (!(o instanceof IntegerMax)) { return false; }
+      IntegerMax that = (IntegerMax) o;
+      return v == that.v;
+    }
+
+    @Override
+    public int hashCode() {
       return v;
     }
   }
