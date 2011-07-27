ad519dd9a4611c3c7a714e7228942a1384288e0d
Alex Radzy <aradzivanovich@gmail.com>
2011-07-27 14:09:55 +0300

    Added interface for fast single-threaded map builder

--- src/main/java/collection/persistent/Map.java
+++ src/main/java/collection/persistent/Map.java
@@ -3,7 +3,8 @@ package collection.persistent;
 import javax.annotation.Nullable;
 import java.util.Iterator;
 
-public interface Map<K, V> extends Listable<Map.Entry<K, V>>, Iterable<Map.Entry<K, V>> {
+public interface Map<K, V> extends Listable<Map.Entry<K, V>>,
+    Iterable<Map.Entry<K, V>> {
   @Nullable
   V get(K key);
 
@@ -31,5 +32,34 @@ public interface Map<K, V> extends Listable<Map.Entry<K, V>>, Iterable<Map.Entry
 
     @Override
     int hashCode();
+  }
+
+  /**
+   * Fast and efficient single-threaded map builder
+   * it uses mutable structures internally to speed up
+   * construction of immutable maps.
+   *
+   * @param <K> Key type.
+   * @param <V> Value type.
+   */
+  interface Builder<K, V> {
+    /**
+     * Add key/value mapping to the builder, replacing
+     * any previously created mapping for the same key.
+     *
+     * @param key   A key.
+     * @param value A value to associate with the key.
+     * @return This builder instance.
+     */
+    Builder<K, V> put(K key, V value);
+
+    /**
+     * Build persistent map from the added keys and values.
+     *
+     * @return Persistent {@link Map map} built from
+     *         the added key/value pairs.
+     * @throws IllegalStateException If a map was built before.
+     */
+    Map<K, V> build();
   }
 }
